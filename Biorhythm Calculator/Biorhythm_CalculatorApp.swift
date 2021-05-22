86a8b6f6ba50791f04894c5d408546012de8132b
Ingo RIchter <ingo.richter+github@gmail.com>
2021-05-21 21:25:00 -0700

    Initial Commit

---
+++ Biorhythm Calculator/Biorhythm_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biorhythm_CalculatorApp.swift
+//  Biorhythm Calculator
+//
+//  Created by Ingo Richter on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Biorhythm_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Biorhythm Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Biorhythm Calculator
+//
+//  Created by Ingo Richter on 5/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
